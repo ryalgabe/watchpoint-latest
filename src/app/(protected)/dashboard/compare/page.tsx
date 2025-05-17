677eccfcbbfb7a843e2170fb650f14a166f7df7f
Gabe Ryal <gaberyal@pop-os.home>
2025-05-17 19:26:35 +0200

    potential fix

--- src/app/(protected)/dashboard/compare/page.tsx
+++ src/app/(protected)/dashboard/compare/page.tsx
@@ -4,11 +4,9 @@ import { WatchComparator } from '@/components/shared/compare/watch-comparator'
 export default async function ComparePage() {
   const watches = await getAllWatches()
 
-
   if (!watches) {
     return <div>No watches found</div>
   }
-
 
   //@ts-ignore
   return <WatchComparator allWatches={watches} />
